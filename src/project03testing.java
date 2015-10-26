05a35e824cc38559d465aaa31a518b328b0cf514
David Gudeman <davidmgudeman@gmail.com>
2015-10-26 09:02:14 -0700

    Going to be starting on remove alterations

--- src/project03testing.java
+++ src/project03testing.java
@@ -5,7 +5,7 @@ public class project03testing
 
    public static void main(String[] args)
    {
-      System.out.println("Hello   two");
+      LazySearchTree test = new LazySearchTree();
 
    }
 
